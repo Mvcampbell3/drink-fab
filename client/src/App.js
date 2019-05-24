adb4ac7ed2c9e021c20d392b0ada62f9f3fe0907
Mvcampbell3 <mvcampbell3@gmail.com>
2019-05-24 18:24:09 -0400

    inital file commits

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Enter the Drinkon</h1>
+    );
+  }
+}
+
+export default App;
